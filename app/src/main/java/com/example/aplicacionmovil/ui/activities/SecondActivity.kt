b16c9b986b48b2c9d923d49ff57eda20e866cbee
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-06-06 19:36:17 -0500

    navigation bar subido por Xavier Aguilar

--- app/src/main/java/com/example/aplicacionmovil/ui/activities/SecondActivity.kt
+++ app/src/main/java/com/example/aplicacionmovil/ui/activities/SecondActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.example.aplicacionmovil.R
 import com.example.aplicacionmovil.databinding.ActivitySecondBinding
 import com.google.android.material.snackbar.Snackbar
 import kotlin.math.log
@@ -27,17 +28,49 @@ class SecondActivity : AppCompatActivity() {
     }
 
     private fun initClass() {
-        var name: String =""
-        intent.extras.let {
-            name = it?.getString("var1")!!
-        }
-        Log.d("UCE","Hola ${name}")
-        binding.vistaTexto.text =  "Bienvenidos " + name.toString()
+        var name: String = ""
+//        intent.extras.let {
+//            name = it?.getString("var1")!!
+//        }
+//        Log.d("UCE","Hola ${name}")
+        binding.vistaTexto.text = "Bienvenidos " + name.toString()
 
         binding.boton.setOnClickListener {
             var intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.inicio -> {
+
+                    var suma : Int = 0
+
+                    for (i in listOf(1,2,3)){
+                        suma += i
+                    }
+                    Snackbar.make(binding.vistaTexto,"La suma es $suma", Snackbar.LENGTH_LONG).show()
+                    true
+                }
+                R.id.favoritos -> {
+                    var suma : Int = 0
+
+                    for (i in listOf(8,12,13)){
+                        suma += i
+                    }
+                    Snackbar.make(binding.vistaTexto,"La suma es $suma", Snackbar.LENGTH_LONG).show()
+
+                    true
+                }
+                R.id.apis -> {
+                    true
+                }
+                else -> false
+            }
+        }
+
     }
+
+
 
 }
