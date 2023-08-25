f2abbcabbc5fe22a63fc7920f6ad79c6257afb7f
°°PRISCI NENITA°° <kevinjkevps4@gmail.com>
2023-08-25 13:19:00 -0500

    pantalla de inicio incorporada la opcion de recuperar cuenta y finalizada la pantalla de regristro subido por Xavier

--- app/src/main/java/com/example/aplicacionmovil/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/aplicacionmovil/ui/activities/MainActivity.kt
@@ -207,15 +207,6 @@ class MainActivity : AppCompatActivity() {
         locationSettingsRequest =
             LocationSettingsRequest.Builder().addLocationRequest(locationRequest).build()
 
-        //registro
-//        auth = Firebase.auth
-//        binding.ingresar.setOnClickListener {
-//            authWithFirebaseEmail(
-//                binding.ingresoCorreo.text.toString(),
-//                binding.ingresoContrasena.text.toString()
-//            )
-//        }
-
         //ingresar
         auth = Firebase.auth
         binding.ingresar.setOnClickListener {
@@ -224,6 +215,14 @@ class MainActivity : AppCompatActivity() {
                 binding.ingresoContrasena.text.toString()
             )
         }
+        //recuperar contraseña
+        auth = Firebase.auth
+        binding.btnOlvido.setOnClickListener {
+            recoveryPasswordWhithEmail(
+                binding.ingresoCorreo.text.toString()
+            )
+        }
+
         binding.btnHuella.setOnClickListener {
             val intent= Intent(this, BiometricActivity::class.java)
             startActivity(intent)
@@ -236,29 +235,6 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun authWithFirebaseEmail(email: String, password: String) {
-        auth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "createUserWithEmail:success")
-                    val user = auth.currentUser
-                    Toast.makeText(
-                        baseContext,
-                        "Authentication success.",
-                        Toast.LENGTH_SHORT,
-                    ).show()
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                    Toast.makeText(
-                        baseContext,
-                        "Authentication failed.",
-                        Toast.LENGTH_SHORT,
-                    ).show()
-                }
-            }
-    }
 
     private fun signInWithEmailAndPassword(email:String, password: String){
         auth.signInWithEmailAndPassword(email, password)
--- app/src/main/java/com/example/aplicacionmovil/ui/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/aplicacionmovil/ui/activities/RegisterActivity.kt
@@ -1,27 +1,60 @@
 package com.example.aplicacionmovil.ui.activities
 
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.SearchManager
 import android.content.Intent
+import android.location.Location
 import com.example.aplicacionmovil.R
 import android.os.Bundle
+import android.os.Looper
+import android.speech.RecognizerIntent
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.biometric.BiometricPrompt
 import androidx.core.content.ContextCompat
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.lifecycle.lifecycleScope
+import com.example.aplicacionmovil.databinding.ActivityMainBinding
 import com.example.aplicacionmovil.databinding.ActivityRegisterBinding
+import com.example.aplicacionmovil.logic.validator.LoginValidator
+import com.example.aplicacionmovil.ui.utilities.MyLocationManager
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.location.LocationSettingsStatusCodes
+import com.google.android.gms.location.Priority
+import com.google.android.gms.location.SettingsClient
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.UUID
 
 class RegisterActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityRegisterBinding
+
+    private lateinit var auth: FirebaseAuth
+    private val TAG = "UCE"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.huellaRegistro.setOnClickListener {
-            val intent= Intent(this, BiometricActivity::class.java)
-            startActivity(intent)
-        }
 
         //registro
         auth = Firebase.auth
@@ -30,6 +63,111 @@ class RegisterActivity : AppCompatActivity() {
                 binding.ingresoCorreo.text.toString(),
                 binding.ingresoContrasena.text.toString()
             )
+        }
+        binding.inicio.setOnClickListener {
+            val intent= Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    private fun authWithFirebaseEmail(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication success.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+    }
+
+
+    private fun recoveryPasswordWhithEmail(email: String){
+        auth.sendPasswordResetEmail(email).addOnCompleteListener{task->
+            if(task.isSuccessful){
+                //de una forma
+                Toast.makeText(
+                    this,
+                    "Correo de recuperacion enviado correctamente",
+                    Toast.LENGTH_SHORT,
+                ).show()
+                // de otra
+                MaterialAlertDialogBuilder(this).apply {
+                    setTitle("Alert")
+                    setMessage("Correo de recuperacion enviado correcftamente")
+                    setCancelable(true)
+                }
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+//        initClass()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    private fun initClass() {
+        binding.ingresar.setOnClickListener {
+
+            val check = LoginValidator().checkLogin(
+                binding.ingresoCorreo.text.toString(),
+                binding.ingresoContrasena.text.toString()
+            )
+
+            if (check) {
+                lifecycleScope.launch(Dispatchers.IO) {
+                    saveDataStore(binding.ingresoCorreo.text.toString())
+                }
+
+                var intent = Intent(
+                    this,
+                    SecondActivity::class.java
+                )
+
+                intent.putExtra(
+                    "var1",
+                    ""
+                )
+
+                intent.putExtra("var2", 2)
+                startActivity(intent)
+            } else {
+                Snackbar.make(
+                    binding.pedOnl, "Usuario o contraseña inválidos",
+                    Snackbar.LENGTH_LONG
+                ).show()
+            }
+        }
+
+    }
+
+    private suspend fun saveDataStore(stringData: String) {
+        dataStore.edit { prefs ->
+            prefs[stringPreferencesKey("user")] = stringData
+            prefs[stringPreferencesKey("session")] = UUID.randomUUID().toString()
+            prefs[stringPreferencesKey("email")] = "dispositivos_moviles@uce.edu.ec"
         }
     }
 }
